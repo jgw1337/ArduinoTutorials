0fc281e245b215415b08f393b664a4164f07c012
jgwang-github <jgwang.github@gmail.com>
2015-09-25 21:17:27 -0700

    SOS (via Blink Test)

---
+++ mayday/maydayBlinkTest.ino
@@ -0,0 +1,46 @@
+int led = 13;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(led, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  s();
+  o();
+  s();
+  space();
+}
+
+void o() {
+  dash();
+  dash();
+  dash();
+}
+
+void s() {
+  dot();
+  dot();
+  dot();
+}
+
+void space() {
+  delay(800);
+}
+
+void dot() {
+  digitalWrite(led, HIGH);
+  delay(200);
+  digitalWrite(led, LOW);
+  delay(200);
+}
+
+void dash() {
+  digitalWrite(led, HIGH);
+  delay(800);
+  digitalWrite(led, LOW);
+  delay(200);
+}
+
