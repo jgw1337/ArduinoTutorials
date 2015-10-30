ec0c677629f3cd4b7174e4582227989af9bd9836
jgwang-github <jgwang.github@gmail.com>
2015-10-29 23:34:42 -0700

    Sumobot : Rudimentary AI

--- sumobot/sumobot.ino
+++ sumobot/sumobot.ino
@@ -31,10 +31,73 @@ void setup() {
 }
 
 void loop() {
-  see_eye("left");
-  see_eye("right");
-  Serial.println("");
-  delay(100);
+  if ( isInRange("right_eye") ) {
+    all_stop();
+  } else {
+    go("right", 150);
+  }
+
+  if ( isInRange("left_eye") ) {
+    go("backward", 150);
+  }
+
+/*
+  if ( isInRange("ramming_eye") ) {
+    ramming_speed();
+  } else {
+    all_stop();
+  }
+*/
+
+  Serial.println();
+  delay(10);
+}
+
+
+/**
+ * Eye Functions
+ */
+boolean isInRange(String eye) {
+  boolean object_detected = false;
+  int eye_echo;
+  int eye_trig;
+
+  eye.toLowerCase();
+
+  if ( eye == "right_eye" ) {
+    eye_echo = right_eye_echo;
+    eye_trig = right_eye_trig;
+  } else if ( eye == "left_eye" ) {
+    eye_echo = left_eye_echo;
+    eye_trig = left_eye_trig;
+  } else {
+    // ramming eye
+  }
+
+  digitalWrite(eye_trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(eye_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(eye_trig, LOW);
+  duration = pulseIn(eye_echo, HIGH);
+  inches = duration / 74 / 2;
+  cm = (duration/2) / 29.1;
+
+  // Three feet is the max range
+  if (inches / 12 <= max_sight_in_feet){
+    object_detected = true;
+  }
+
+  int max_text_width = 25;
+  String padding;
+  String strDisplay;
+  strDisplay = eye + " eye : " + inches + " in.";
+  for (int i = 0; i < max_text_width - strDisplay.length(); i++) {
+    padding += " ";
+  }
+  Serial.print(strDisplay + padding + object_detected + " ; ");
+
+  return object_detected;
 }
 
 
@@ -46,46 +109,6 @@ void init_eyes() {
   pinMode(left_eye_echo, INPUT);
   pinMode(right_eye_trig, OUTPUT);
   pinMode(right_eye_echo, INPUT);
-}
-
-void see_eye(String dir) {
-  int eye_echo;
-  int eye_trig;
-  int max_text_width = 50;
-  String padding;
-
-  if ( dir == "right") {
-    dir = "Right";
-    eye_echo = right_eye_echo;
-    eye_trig = right_eye_trig;
-  } else {
-    dir = "Left";
-    eye_echo = left_eye_echo;
-    eye_trig = left_eye_trig;
-  }
-  
-  digitalWrite(eye_trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(eye_trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(eye_trig, LOW);
-  duration = pulseIn(eye_echo, HIGH);
-  inches = duration / 74 / 2;
-  cm = (duration/2) / 29.1;
-
-  String strDisplay;
-  strDisplay = dir + " eye : " + inches + " in.";
-
-  // Three feet is the max range
-  if (inches / 12 >= max_sight_in_feet){
-    strDisplay += ".. out of range.";
-  }
-
-  for (int i = 0; i < max_text_width - strDisplay.length(); i++) {
-    padding += " ";
-  }
-
-  Serial.print(strDisplay + padding);
 }
 
 
@@ -101,43 +124,70 @@ void init_feet() {
   pinMode(motor_right_pwm, OUTPUT);
 }
 
-void spin(String dir) {
-  int motor_fwd_in1;
-  int motor_fwd_in2;
-  int motor_fwd_pwm;
-  int motor_bkwd_in1;
-  int motor_bkwd_in2;
-  int motor_bkwd_pwm;
 
-  if ( dir == "right") {
-    dir = "Right";
-    motor_fwd_in1 = motor_left_in1;
-    motor_fwd_in2 = motor_left_in2;
-    motor_fwd_pwm = motor_left_pwm;
-    motor_bkwd_in1 = motor_right_in1;
-    motor_bkwd_in2 = motor_right_in2;
-    motor_bkwd_pwm = motor_right_pwm;
-  } else {
+/**
+ * Motor Functions
+ */
+void go(String dir, int speed) {
+  dir.toLowerCase();
+
+  if ( dir == "forward" ) {
+    dir = "Forward";
+    digitalWrite(motor_right_in1, HIGH);
+    digitalWrite(motor_right_in2, LOW);
+
+    digitalWrite(motor_left_in1, HIGH);
+    digitalWrite(motor_left_in2, LOW);
+
+  } else if ( dir == "backward" ) {
+    dir = "Backward";
+    digitalWrite(motor_right_in1, LOW);
+    digitalWrite(motor_right_in2, HIGH);
+
+    digitalWrite(motor_left_in1, LOW);
+    digitalWrite(motor_left_in2, HIGH);
+
+  } else if ( dir == "left" ) {
     dir = "Left";
-    motor_fwd_in1 = motor_right_in1;
-    motor_fwd_in2 = motor_right_in2;
-    motor_fwd_pwm = motor_right_pwm;
-    motor_bkwd_in1 = motor_left_in1;
-    motor_bkwd_in2 = motor_left_in2;
-    motor_bkwd_pwm = motor_left_pwm;
+    digitalWrite(motor_right_in1, HIGH);
+    digitalWrite(motor_right_in2, LOW);
+
+    digitalWrite(motor_left_in1, LOW);
+    digitalWrite(motor_left_in2, HIGH);
+
+  } else if ( dir == "right" ) {
+    dir = "Right";
+    digitalWrite(motor_right_in1, LOW);
+    digitalWrite(motor_right_in2, HIGH);
+
+    digitalWrite(motor_left_in1, HIGH);
+    digitalWrite(motor_left_in2, LOW);
+
+  } else {
+    dir = "Spin Right";
+    
   }
 
-  // Step forward
-  digitalWrite(motor_fwd_in1, HIGH);
-  digitalWrite(motor_fwd_in2, LOW);
-  analogWrite(motor_fwd_pwm, 255);
+  // Right
+  analogWrite(motor_right_pwm, speed);
 
-  // Step backward
-  digitalWrite(motor_bkwd_in1, LOW);
-  digitalWrite(motor_bkwd_in2, HIGH);
-  analogWrite(motor_bkwd_pwm, 255);
+  // Left
+  analogWrite(motor_left_pwm, speed);
+
+  int max_text_width = 30;
+  String padding;
+  String strDisplay;
+  strDisplay = " dir : " + dir + "; speed : " + speed;
+  for (int i = 0; i < max_text_width - strDisplay.length(); i++) {
+    padding += " ";
+  }
+  Serial.print(strDisplay + padding + " ; ");
 }
 
+
+/**
+ * Motor Functions
+ */
 void all_stop() {
   digitalWrite(motor_left_in1, LOW);
   digitalWrite(motor_left_in2, LOW);
@@ -145,12 +195,10 @@ void all_stop() {
   digitalWrite(motor_right_in2, LOW);
 }
 
-void ramming_speed() {
-  digitalWrite(motor_left_in1, HIGH);
-  digitalWrite(motor_left_in2, LOW);
-  analogWrite(motor_left_pwm, 255);
-  digitalWrite(motor_right_in1, HIGH);
-  digitalWrite(motor_right_in2, LOW);
-  analogWrite(motor_right_pwm, 255);
-}
 
+/**
+ * Motor Functions
+ */
+void ramming_speed() {
+  go("forward", 255);
+}
